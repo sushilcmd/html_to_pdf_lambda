91a9a0f8b251b308b377f45de5db1fb48855e061
sushil kumar singh <sushilsingh8853@gmail.com>
2021-03-27 14:50:41 +0530

    Update response.js
    Update response header

--- src/response.js
+++ src/response.js
@@ -1,20 +1,21 @@
-
-const success = (bodyData) => {
-    return {
-        headers: {
+const header={
             "Content-type": "application/json",
-            "Access-Control-Allow-Origin": "*",
-        },
+            'Access-Control-Allow-Credentials': true,
+            "Access-Control-Allow-Methods": "OPTIONS,POST",
+            "Access-Control-Allow-Headers":"Content-Type,Authorization,X-Amz-Date,X-Api-Key,X-Amz-Security-Token"
+        }
+
+const success = (bodyData,origin) => {
+    return {
+        headers:{...header, "Access-Control-Allow-Origin": origin},
         statusCode: 200,
         body: JSON.stringify(bodyData),
     }
 }
 
-const error = (err) => {
+const error = (err,origin) => {
     return {
-        headers: {
-            "Content-type": "application/json",
-        },
+        headers:{...header, "Access-Control-Allow-Origin": origin},
         statusCode: 500,
         body: JSON.stringify(err)
     };
